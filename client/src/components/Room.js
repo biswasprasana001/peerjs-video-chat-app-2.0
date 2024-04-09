790e28ac91e33cb4c7d98dfe2c9af8144634ead9
biswasprasana001 <biswasprasana004@gmail.com>
2024-04-09 18:35:09 +0530

    Fixed Leaving Room Bug

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,35 +1,29 @@
 import React, { useEffect, useState } from 'react';
-// import { useRef } from 'react';
 import { useSocket } from '../context/SocketProvider';
 import { useParams } from 'react-router-dom';
 import Peer from 'peerjs';
 
 const VideoChat = () => {
-  // const [peers, setPeers] = useState({});
   const [streams, setStreams] = useState([]);
-  // const videoGridRef = useRef();
+  const [peers, setPeers] = useState([]);
 
   const { roomId } = useParams();
   const socket = useSocket();
 
   useEffect(() => {
     const myPeer = new Peer();
-    // const myVideo = document.createElement('video');
-    // myVideo.muted = true;
     navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     }).then(stream => {
-      // addVideoStream(myVideo, stream);
       setStreams(prevStreams => [...prevStreams, stream]);
 
-      // myPeer.on('call', call => {
-      //   call.answer(stream);
-      //   const video = document.createElement('video');
-      //   call.on('stream', userVideoStream => {
-      //     addVideoStream(video, userVideoStream);
-      //   });
-      // });
+      myPeer.on('call', call => {
+        call.answer(stream);
+        call.on('stream', userVideoStream => {
+          setStreams(prevStreams => [...prevStreams, userVideoStream]);
+        });
+      });
 
       socket.on('user-connected', peerId => {
         connectToNewUser(peerId, stream);
@@ -38,53 +32,42 @@ const VideoChat = () => {
     });
 
     socket.on('user-disconnected', peerId => {
-      // if (peers[userId]) peers[userId].close();
       console.log("user-disconnected", peerId);
+      setPeers(prevPeers => prevPeers.filter(peer => peer !== peerId));
+      setStreams(prevStreams => prevStreams.filter(stream => stream.peerId !== peerId));
     });
 
     myPeer.on('open', peerId => {
       socket.emit('join-room', roomId, peerId);
-      // console.log("roomId", roomId, "peerId", peerId, "socket", socket);
     });
 
-    // function connectToNewUser(peerId, stream) {
-    //   const call = myPeer.call(peerId, stream);
-    //   const video = document.createElement('video');
-    //   call.on('stream', userVideoStream => {
-    //     addVideoStream(video, userVideoStream);
-    //   });
-    //   call.on('close', () => {
-    //     video.remove();
-    //   });
-    //   setPeers(prevPeers => ({ ...prevPeers, [peerId]: call }));
-    // }
-
-    // function addVideoStream(video, stream) {
-    //   video.srcObject = stream;
-    //   video.addEventListener('loadedmetadata', () => {
-    //     video.play();
-    //   });
-    //   videoGridRef.current.append(video);
-    // }
+    function connectToNewUser(peerId, stream) {
+      const call = myPeer.call(peerId, stream);
+      call.on('stream', userVideoStream => {
+        setStreams(prevStreams => [...prevStreams, userVideoStream]);
+        setPeers(prevPeers => [...prevPeers, peerId]);
+        console.log("userVideoStream", userVideoStream); // Why is it running two times. It should be called once as only one user is being called & streams from it being received
+      });
+      // call.on('close', () => {
+      // setPeers(prevPeers => prevPeers.filter(peer => peer !== peerId));
+      // setStreams(prevStreams => prevStreams.filter(stream => stream.peerId !== peerId));
+      // });
+    }
 
     return () => {
       myPeer.off('open');
+      myPeer.off('call');
       socket.off('user-connected');
       socket.off('join-room');
       socket.off('user-disconnected');
       socket.disconnect();
       myPeer.destroy();
+
     };
   }, []);
 
   return (
     <div>
-      {/* {streams.map((stream, index) => ( */}
-      {/* Why the below video is displayed twice if there is only one stream? */}
-      {/* <video key={index} autoPlay playsInline ref={video => { */}
-      {/* if (video) video.srcObject = stream; */}
-      {/* }} /> */}
-      {/* ))} */}
       {streams.map((stream, index) => (
         stream.active && <video key={index} autoPlay playsInline ref={video => {
           if (video) video.srcObject = stream;
